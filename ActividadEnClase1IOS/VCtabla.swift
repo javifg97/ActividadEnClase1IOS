956ab42cbc5a3377007646918853766c9957014e
javier.fernandez3 <javier.fernandez3@live.u-tad.com>
2017-03-27 20:36:40 +0200

    27/3/17

---
+++ ActividadEnClase1IOS/VCtabla.swift
@@ -0,0 +1,37 @@
+//
+//  VCtabla.swift
+//  ActividadEnClase1IOS
+//
+//  Created by Javier Fernández García on 27/3/17.
+//  Copyright © 2017 U-tad. All rights reserved.
+//
+
+import UIKit
+
+class VCtabla: UIViewController {
+    
+    @IBOutlet var tbMiTable: UITableView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
